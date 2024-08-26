5dddab09d5b329a9b7d20eeac5cb47f9034712e0
willywairagu <wwairagu@sanku.com>
2024-08-26 21:17:31 +0300

    allowed origins config

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -135,7 +135,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-  'http://localhost:3000'
+  'http://localhost:3000', 
+  'http://willywairagu.com',
+  'https://willywairagu.com'
 ]
 
 
